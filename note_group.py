7dd105c15c1de6ff48da7ef020a75675f257aced
Kelly Herron <kherron@luc.edu>
2017-09-20 00:10:06 -0500

    added topological sort to NoteGroup, added tests for all mentions, all topics, and topological sort

--- note_group.py
+++ note_group.py
@@ -1,4 +1,5 @@
 from note_contents import Note
+from collections import deque
 
 class NoteGroup(object):
 
@@ -23,3 +24,31 @@ class NoteGroup(object):
 
     def with_topic(self, topic):
         return list(filter(lambda x: x.has_topic(topic), self.notes))
+
+    def topo_sort(self):
+        ids = list(map(lambda x: x.unique_id, self.notes))
+        
+        in_degree = [0] * len(self.notes)
+        #list of empty lists, one for each note
+        graph = [[] for i in range(len(self.notes))]
+
+        #construct graph and in_degree array
+        for i, note in enumerate(self.notes):
+            graph[i].extend(note.references)
+            for ref in note.references:
+                in_degree[ids.index(ref)] += 1
+
+        #initialize queue with indices of notes not referenced
+        in_degree_zero = deque(i for i, x in enumerate(in_degree) if x == 0)
+        t_sorted = []
+
+        #https://courses.cs.washington.edu/courses/cse326/03wi/lectures/RaoLect20.pdf
+        while(len(in_degree_zero) > 0):
+            note_index = in_degree_zero.popleft()
+            t_sorted.append(self.notes[note_index])
+            for adjacent in graph[note_index]:
+                in_degree[ids.index(adjacent)] -= 1
+                if in_degree[ids.index(adjacent)] == 0:
+                    in_degree_zero.append(ids.index(adjacent))
+
+        return t_sorted
--- note_group_test.py
+++ note_group_test.py
@@ -11,6 +11,17 @@ class TestNoteGroup(unittest.TestCase):
         for j in range(1, i+1):
             notes[j].add_mentions(i)
             notes[j].add_topics(i+1)
+    #add ids and references
+    notes[0].create_id("a")
+    notes[2].create_id("c")
+    notes[3].create_id("d")
+    notes[4].create_id("e")
+    notes[5].create_id("f")
+    notes[0].add_references("e", "f")
+    notes[1].add_references("a")
+    notes[2].add_references("d")
+    notes[4].add_references("c")
+    notes[5].add_references("c", "e")
     ng = NoteGroup(notes)
 
     #tests use set to verify that all elements are the same, order does not matter
@@ -28,7 +39,19 @@ class TestNoteGroup(unittest.TestCase):
         self.assertEqual(set(self.ng.with_topic(5)), set(self.notes[1:5]))
         self.assertEqual(set(self.ng.with_topic(4)), set(self.notes[1:4]))
         self.assertEqual(set(self.ng.with_topic(3)), set(self.notes[1:3]))
-        self.assertEqual(self.ng.with_topic(2)[0], self.notes[1])        
+        self.assertEqual(self.ng.with_topic(2)[0], self.notes[1])
+
+    def test_mentions(self):
+        self.assertEqual(self.ng.mentions, set([1, 2, 3, 4]))
+
+    def test_topics(self):
+        self.assertEqual(self.ng.topics, set([2, 3, 4, 5]))
+
+    def test_topo_sort(self):
+        expected_sort = ["b", "a", "f", "e", "c", "d"]
+        ng_topo_sort = list(map(lambda x: x.file_name, self.ng.topo_sort()))
+        self.assertEqual(expected_sort, ng_topo_sort)
+        
 
 if __name__ == '__main__':
     unittest.main()
