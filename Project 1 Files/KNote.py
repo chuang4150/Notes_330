5e757a303c7de652a2518c12d51a0034042a4dc0
Chris Huang <chuang4150@gmail.com>
2017-09-21 21:43:04 -0500

    Add files via upload
    finishing touches on UI, TODO:TOPO_SORT

--- Project 1 Files/KNote.py
+++ Project 1 Files/KNote.py
@@ -38,11 +38,6 @@ def main():
     files=get_file_names()
     print("Files that will be analyzed: ",files)
 
-
-
-
-
-
     # symbol = input('Enter the symbol you are searching for:')
     notes=[]
     for fileName in files:
@@ -57,53 +52,50 @@ def main():
         topics=find.find_identifiers(searched_note, '#')
         references=find.find_identifiers(searched_note,'^')
         unique_id=find.find_identifiers(searched_note,'!')
+        urls=find.find_urls(searched_note)
         if len(unique_id) > 0:
             notes[len(notes)-1].create_id(unique_id[0][1:])
-            print (unique_id[0][1:])
-        urls=find.find_urls(searched_note)
+            print ("\nUnique Note ID:" + unique_id[0][1:])
+
 
         notes[len(notes)-1].add_mentions(*[e[1:] for e in mentions])
         notes[len(notes)-1].add_topics(*[e[1:] for e in topics])
         notes[len(notes)-1].add_references(*[e[1:] for e in references])
         notes[len(notes)-1].add_urls(*urls)
         # print (mentions)
-        # print (topics)
         # print (references)
-        # print (urls)
-        # print ("Mention found in " + searched_note.title)
+        print("Url(s) found:", urls)
+        print("Mention(s) found:", mentions)
 
     compilation=NoteGroup(notes)
     containingMentions=compilation.with_mentions()
 
     # TODO: Case statement
-    print("These are the notes containing mentions: ")
-    for mention in containingMentions:
-        print(mention)
-
+    print("\nThese are the notes containing mentions: \n")
+    # for mention in containingMentions:
+    #     print(mention)
     for mention in compilation.mentions:
-        print(mention)
+        # print(mention)
 
         for note in compilation.with_mention(mention):
-            print(note)
 
+            print(note, ':', mention)
+
+    print("\nThese are the notes containing topics: \n")
     for topic in compilation.topics:
-        print (topic)
+        # print (topic)
 
         for note in compilation.with_topic(topic):
-            print (note)
 
-    # print("These are the notes containing topics: ")
-    # for topics in containingMentions:
-    #     print (topics)
-    #
-    # print("These are the notes containing references: ")
-    # for references in containingMentions:
-    #     print (references)3
+            print (note,':', topic)
 
     contine_search = input("Would you like to redo the analysis?")
     if contine_search in ('y', 'yes'):
+            os.system('clear')
             main()
+
     else:
             print ('Program terminating')
+            os.system('clear')
 
 main()
